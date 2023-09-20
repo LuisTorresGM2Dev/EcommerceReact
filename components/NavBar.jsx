276fe68f348b9e75bb7fca01cdd510049e217270
GM2 Academy <gm2academy@MacBook-Pro-de-Tecnico.local>
2023-09-20 10:24:30 -0300

    feat:add button

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,11 +2,14 @@ import "../src/navbar.css";
 
 function Navbar() {
     return (
-        <div>
+        <div className="containerNav">
             <div className="logo">
                 <img src="../images/Vector-g.svg" alt="" />
                 <img src="../images/Vector-m.svg" alt="" />
                 <img src="../images/Vector-2.svg" alt="" />
+            </div>
+            <div className="changeMode">
+                <button className="change">Change Mode</button>
             </div>
         </div>
     );
